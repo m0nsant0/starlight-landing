bd6a5738ac623e6a04cf498a3117167e28061e30
ashr1c <monsanto68@gmail.com>
2025-12-07 20:38:22 -0400

    Add landing page files

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+const prompt = document.querySelector('.scroll-prompt');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) prompt.style.opacity = '0';
+});
